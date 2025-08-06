e78dafd2b7c9613b51286d5c784265f88d25b482
Vinicius Ledesma <viniciusjrl@me.com>
2025-08-06 12:46:49 -0300

    fix: Corrigindo o carregamento de imagens do banco de dados

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Adicione esta seção de 'images'
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/w500/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
